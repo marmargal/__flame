31e4c24a8c5536aab0e37c5a042eebb2d143bbc9
Inmaculada Martín Galván <inma.martin.galvan@hotmail.com>
2019-01-29 21:34:37 +0100

    Prueba PyQt5

--- Workspace/Flame/Scraping/Lefties.py
+++ Workspace/Flame/Scraping/Lefties.py
@@ -1,8 +1,11 @@
 import re
 import json
+import string
+import sys
+#from PyQt4 import QtGui
 from bs4 import BeautifulSoup
 from pip._vendor import requests
-import string
+
 
 prendaLefties = "https://www.lefties.com/es/women/novedades-woman/camiseta-mickey-c1029507p501184069.html"
 prendaHM = "https://www2.hm.com/es_es/productpage.0651110001.html"
@@ -11,26 +14,45 @@ prendaZara = "https://www.zara.com/es/es/cazadora-acolchada-estampado-animal-p04
 urlLefties = "https://www.lefties.com/es/#woman"
 
 
+# javascript
+def obtener_prenda(urlPrenda):
+    req = requests.get(urlPrenda)
+    data = req.text
+    soup = BeautifulSoup(data,'html.parser')
+    
+    prenda = []
+    return prenda
+
+
 def obtener_todas_las_prendas(urlWeb):
     req = requests.get(urlWeb)
     data = req.text
     soup = BeautifulSoup(data,'html.parser')
     
-    res = []
-    
+    res = [] # almacenamos las URLS
     
 #     for i in soup.find_all("script", type="text/javascript"):
 #         print i
 #         print ("=======================================")
+#     script = soup.html.find_next_sibling('script', re.compile(r"\$\(document\)\.ready"))
+#     pattern = re.compile("(\w+): '(.*?)'")
+#     fields = dict(re.findall(pattern, script.text))
+#     print fields['woman']
     
-    script = soup.html.find_next_sibling('script', re.compile(r"\$\(document\)\.ready"))
-    pattern = re.compile("(\w+): '(.*?)'")
-    fields = dict(re.findall(pattern, script.text))
-    print fields['woman']
+    #"sidemenu-list-item new-collection parent"
+    #"sidemenu-list-item"
+    for i in soup.find_all(class_="sidemenu-list-item new-collection parent"):
+        print (i.gettext())
+      
+    for r in res:
+        print r
+    print res
+    return res
 
 obtener_todas_las_prendas(urlLefties)
 
 # div id = "product-container"
+# por el json del javascript
 def obtener_prenda_lefties(urlPrenda):
     req = requests.get(urlPrenda)
     data = req.text
---
+++ Workspace/Flame/Scraping/Lefties02.py
@@ -0,0 +1,26 @@
+import sys 
+from PyQt5.QtWidgets import QApplication 
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage # Webkit
+import bs4 as bs
+import urllib.request
+
+class Client(QWebEnginePage):
+    def __init__(self,url):
+        self.app = QApplication(sys.argv)
+        #QWebEnginePage.__init__(self)
+        QWebEngineView.__init__(self)
+        self.loadFinished.connect(self.on_page_load)
+        self.load(QUrl(url))
+        self.app.exec_()
+        
+    def on_page_load(self):
+        self.app.quit()
+        
+url = 'https://pythonprogramming.net/parsememcparseface/' #'https://www.lefties.com/es/#woman'
+client_response = Client(url)
+#source = client_response.toHtml()
+#soup = bs.BeautifulSoup(source,'lxml')
+print(client_response.toHtml())
+#print(soup.prettify())
+    
